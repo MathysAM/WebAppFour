abf937b511bdc8d5ed3d27c69cbe823a2de6e457
MathysAM <m.doulcier@am-automation.com>
2025-05-21 00:11:25 +0200

    Modif ordre appelle

--- Blazzor/WebAppFour/Four/Program.cs
+++ Blazzor/WebAppFour/Four/Program.cs
@@ -10,7 +10,9 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddScoped<MqttService>();
+
 await builder.Build().RunAsync();
 
 
-builder.Services.AddScoped<MqttService>();
+
